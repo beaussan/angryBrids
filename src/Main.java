7285d14fbbcff5336476cd9c284e7fd6ac8eb61b
Benjamin D'HOOP <benjamin.d-hoop@etudiant.univ-lille1.fr>
2015-09-22 11:47:34 +0200

    Clean git + Ajout de la fenetre dans Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import javax.swing.JFrame;
+
+import main.Fenetre;
+import main.Jeu;
 
 public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("Hello world! This is a test");
+		JFrame f = new JFrame("Test");
+		Jeu j = new Jeu();
+		Fenetre fen = new Fenetre(500, 700);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(fen);
+		f.pack();
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+		
 	}
+	
 }
