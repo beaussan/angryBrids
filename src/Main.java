47ba99892367164c2d738caae1607a2507b288fa
Benjamin D'HOOP <benjamin.d-hoop@etudiant.univ-lille1.fr>
2015-09-22 11:49:50 +0200

    Changing dimensions of the window

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame f = new JFrame("Test");
 		Jeu j = new Jeu();
-		Fenetre fen = new Fenetre(500, 700);
+		Fenetre fen = new Fenetre(700, 500);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.add(fen);
 		f.pack();
