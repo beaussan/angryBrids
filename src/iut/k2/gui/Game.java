5cae69bd08cd873521e01d5162e7d3f22fb16554
Benjamin D'HOOP <benjamin.d-hoop@etudiant.univ-lille1.fr>
2015-09-29 15:02:37 +0200

    removed Game from main

--- src/iut/k2/gui/Game.java
+++ src/iut/k2/gui/Game.java
@@ -2,10 +2,27 @@ package iut.k2.gui;
 
 import iut.k2.data.Obstacle;
 import iut.k2.data.Pecker;
+import iut.k2.physics.Coordinate2D;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-	private Pecker p;
+	private Pecker pecker;
 	private List<Obstacle> listeObstacles;
+	
+	public Game(){
+		pecker = new Pecker(new Coordinate2D(20, 20));
+		listeObstacles = new ArrayList<Obstacle>();
+	}
+
+	public Pecker getPecker() {
+		return pecker;
+	}
+
+	public List<Obstacle> getListeObstacles() {
+		return listeObstacles;
+	}
+	
+	
 }
