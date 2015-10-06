60387e0d7aeadfacae6c3953a020ec8aeded3656
Nicolas Beaussart <nic.beaussart@gmail.com>
2015-10-06 14:28:00 +0200

    Removing derivate

--- src/iut/k2/physics/functions/ArchimedeSpiral.java
+++ src/iut/k2/physics/functions/ArchimedeSpiral.java
@@ -1,6 +1,7 @@
 package iut.k2.physics.functions;
 
-import static java.lang.Math.*;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 
 /**
  * Created by PROPRIETAIRE on 29/09/2015.
@@ -11,13 +12,13 @@ public class ArchimedeSpiral extends ParamCurve {
     public final double a = 400;
 
     @Override
-    public double getTMin() {
-        return tMin;
+    public double getTMax() {
+        return tMax;
     }
 
     @Override
-    public double getTMax() {
-        return tMax;
+    public double getTMin() {
+        return tMin;
     }
 
     @Override
@@ -30,13 +31,4 @@ public class ArchimedeSpiral extends ParamCurve {
         return a*(t* sin(t));
     }
 
-    @Override
-    public double xPrim(double t) {
-        return (cos(t)-t* sin(t))/ sqrt(1 + pow(t, 2));
-    }
-
-    @Override
-    public double yPrim(double t) {
-        return (t*cos(t)-sin(t))/ sqrt(1 + pow(t, 2));
-    }
 }
