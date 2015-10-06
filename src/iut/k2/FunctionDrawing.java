47e4d83ecc8f889cf57f31e6697539f2ed4f2ede
Nicolas Beaussart <nic.beaussart@gmail.com>
2015-10-06 13:45:16 +0200

    Adding Drawing for rendu

--- src/iut/k2/FunctionDrawing.java
+++ src/iut/k2/FunctionDrawing.java
@@ -1,7 +1,7 @@
 package iut.k2;
 
 import iut.k2.gui.renderfunc.DrawingPlace;
-import iut.k2.physics.functions.Square;
+import iut.k2.physics.functions.ArchimedeSpiral;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,7 +19,7 @@ public class FunctionDrawing {
             public void run() {
                 JFrame jf = new JFrame("Hello curve");
                 
-                DrawingPlace dp = new DrawingPlace(new Square());
+                DrawingPlace dp = new DrawingPlace(new ArchimedeSpiral());
                 dp.setPreferredSize(new Dimension(250, 250));
                 jf.add(dp);
                 jf.setLocationRelativeTo(null);
