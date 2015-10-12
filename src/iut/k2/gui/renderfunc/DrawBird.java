cf186c3263cf5d48bc5b0c61ac664e258477c2d1
Setchan <bastien.dusart@outlook.com>
2015-10-12 17:52:21 +0200

    test perpendicular 1.

--- src/iut/k2/gui/renderfunc/DrawBird.java
+++ src/iut/k2/gui/renderfunc/DrawBird.java
@@ -1,6 +1,8 @@
 package iut.k2.gui.renderfunc;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
+
 
 /**
  * Created by PROPRIETAIRE on 29/09/2015.
@@ -28,9 +30,43 @@ public class DrawBird {
             yTo = y + (int) (40 * Math.sin(angle + Math.PI));
         }*/
 
-        int lineX = (int) (x - (40 * Math.cos(angle)));
-        int lineY = (int) (y - (40 * Math.sin(angle)));
+        int lineX = (int) x;
+        int lineY = (int) y;
         g.drawLine(lineX, lineY, xTo, yTo);
+        
+        /*int[] x2={(int)x,(int)x,(int)x+10};
+		int[] y2={(int)(y-20),(int)y,(int)(y-10)};
+		g.drawPolygon(x2,y2,x2.length);
+		g.fillPolygon(x2,y2,x2.length);*/
+    	
+        
+        
+        
+        Point2D h = new Point(xTo, yTo);
+		Point2D i = new Point(lineX, lineY);
+				
+		Point2D e = topoints(h,i)[0];
+		Point2D f = topoints(h,i)[1];
+   		/*double a = (i.getY()-h.getY()) / (i.getX()-h.getX());
+    	double coef = -1 / a;
+    	double pos  = i.getY() - coef * i.getX();
+    	
+    	double y3= coef*(i.getX()+10)+pos;
+		double y4= coef*(i.getX()-10)+pos;
+		
+        g.drawLine((int)(i.getX()+10), (int)y3, (int)(i.getX()-10), (int)y4);*/
+
+        /*double angle2 = getAngle((int)e.getX(), (int)e.getY(), (int)f.getX(), (int)f.getY());
+
+
+		int[] x2={(int)h.getX(),(int)e.getX(),(int)f.getX()};
+		int[] y2={(int)h.getY(),(int)e.getY(),(int)f.getY()};*/
+		/*g.drawPolygon(x2,y2,x2.length);
+		g.fillPolygon(x2,y2,x2.length);*/
+        
+        g.drawLine((int)e.getX(), (int)e.getY(), (int)f.getX(), (int)f.getY());
+       /* g.drawLine((int) x - (SIZE_BIRD / 2), (int) y - (SIZE_BIRD / 2), (int)x, (int)y);*/
+
 
         g.setColor(c);
     }
@@ -42,4 +78,42 @@ public class DrawBird {
     public static double getAngleRev(double x, double y, double xNext, double yNext) {
         return Math.atan2(xNext - x, yNext - y);
     }
+    
+    	
+	public static double[] getPerpendiculaire(Point2D a, Point2D b) {
+		return getPerpendiculaire(lineEquation(a, b), b);
+	}
+
+	public static double[] getPerpendiculaire(double eq, Point2D b) {
+		double coef = -1 / eq;
+		double pos  = b.getY() - coef * b.getX();
+		return new double[] { coef, pos };
+	}
+	
+	public static double lineEquation(Point2D p1, Point2D p2) {
+		/*if (p1.getX() == p2.getX()) return (Double) null;*/
+		double a = (p2.getY()-p1.getY()) / (p2.getX()-p1.getX());
+		return a;
+	}
+
+  
+		
+	public static Point2D[] topoints(Point2D A, Point2D B){
+			
+		double a;
+		double b;
+		a=getPerpendiculaire(A, B)[0];
+		b=getPerpendiculaire(A, B)[1];
+		
+		double x=(B.getX()+5);
+		double x2=(B.getX()-5);
+		
+		double y= a*x+b;
+		double y2= a*x2+b;
+
+		return new Point2D[]{new Point((int) x,(int) y),new Point((int) x2,(int) y2)};
+			
+	}
+	
+
 }
