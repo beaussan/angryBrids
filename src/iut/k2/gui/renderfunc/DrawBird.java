013b36917de25970d6aae1caa9ff414f8dcd5198
dusartb <dsfjsdfjsdf@gmail.com>
2015-10-13 14:27:50 +0200

    bird is ok

--- src/iut/k2/gui/renderfunc/DrawBird.java
+++ src/iut/k2/gui/renderfunc/DrawBird.java
@@ -29,32 +29,26 @@ public class DrawBird {
         int xTo = (int) x + (int) (SIEE_ARROW * Math.cos(angle));
         int yTo = (int) y + (int) (SIEE_ARROW * Math.sin(angle));
 
-  /*      if (xTo < x) {
-            xTo = x + (int) (40 * Math.cos(angle + Math.PI));
-            yTo = y + (int) (40 * Math.sin(angle + Math.PI));
-        }*/
 
         int lineX = (int) x;
         int lineY = (int) y;
-        
-        /*int[] x2={(int)x,(int)x,(int)x+10};
-		int[] y2={(int)(y-20),(int)y,(int)(y-10)};
-		g.drawPolygon(x2,y2,x2.length);
-		g.fillPolygon(x2,y2,x2.length);*/
 
 
-        Point2D h = new Point(xTo, yTo);
-		Point2D i = new Point(lineX, lineY);
+       // Point2D h = new Point(xTo, yTo);
+		//Point2D i = new Point(lineX, lineY);
 				
-		Point2D e = topoints(h,i)[0];
-		Point2D f = topoints(h,i)[1];
+		double ex = topoints(xTo, yTo,lineX, lineY)[0];
+		double ey = topoints(xTo, yTo,lineX, lineY)[1];
+		double fx = topoints(xTo, yTo,lineX, lineY)[2];
+		double fy = topoints(xTo, yTo,lineX, lineY)[3];
 
-        double ang = getAngle(x, y, e.getX(), e.getY());
+
+        double ang = getAngle(x, y, ex, ey);
 
         int xBot = (int) x + (int) (SIZE_SIDE * Math.cos(ang));
         int yBot = (int) y + (int) (SIZE_SIDE * Math.sin(ang));
 
-        double ang2 = getAngle(x, y, f.getX(), f.getY());
+        double ang2 = getAngle(x, y, fx, fy);
 
         int xTop = (int) x + (int) (SIZE_SIDE * Math.cos(ang2));
         int yTop = (int) y + (int) (SIZE_SIDE * Math.sin(ang2));
@@ -70,25 +64,6 @@ public class DrawBird {
             g.fillPolygon(new int[]{xTo, xTop, xBot}, new int[]{yTo, yTop, yBot}, 3);
         }
 
-       /* g.drawLine((int) x - (SIZE_BIRD / 2), (int) y - (SIZE_BIRD / 2), (int)x, (int)y);*/
-
-
-   		/*double a = (i.getY()-h.getY()) / (i.getX()-h.getX());
-        double coef = -1 / a;
-    	double pos  = i.getY() - coef * i.getX();
-
-    	double y3= coef*(i.getX()+10)+pos;
-		double y4= coef*(i.getX()-10)+pos;
-
-        g.drawLine((int)(i.getX()+10), (int)y3, (int)(i.getX()-10), (int)y4);*/
-
-        /*double angle2 = getAngle((int)e.getX(), (int)e.getY(), (int)f.getX(), (int)f.getY());
-
-
-		int[] x2={(int)h.getX(),(int)e.getX(),(int)f.getX()};
-		int[] y2={(int)h.getY(),(int)e.getY(),(int)f.getY()};*/
-		/*g.drawPolygon(x2,y2,x2.length);
-		g.fillPolygon(x2,y2,x2.length);*/
         g.setColor(COLOR_BODY);
         if (IS_SKELETON) {
             g.drawOval((int) x - (SIZE_BIRD / 2), (int) y - (SIZE_BIRD / 2), SIZE_BIRD, SIZE_BIRD);
@@ -108,37 +83,45 @@ public class DrawBird {
     }
     
     	
-	public static double[] getPerpendiculaire(Point2D a, Point2D b) {
-		return getPerpendiculaire(lineEquation(a, b), b);
+	public static double[] getPerpendiculaire(double x1, double y1, double x2, double y2) {
+		System.out.println(lineEquation(x1, y1, x2, y2));
+		//System.out.println(getPerpendiculaire(lineEquation(x1, y1, x2, y2), x2, y2)[1]);
+		return getPerpendiculaire(lineEquation(x1, y1, x2, y2), x2, y2);
 	}
 
-	public static double[] getPerpendiculaire(double eq, Point2D b) {
+	public static double[] getPerpendiculaire(double eq, double x2, double y2) {
 		double coef = -1 / eq;
-		double pos  = b.getY() - coef * b.getX();
+		double pos  = y2 - coef * x2;
 		return new double[] { coef, pos };
 	}
 	
-	public static double lineEquation(Point2D p1, Point2D p2) {
+	public static double lineEquation(double x1, double y1, double x2, double y2) {
 		/*if (p1.getX() == p2.getX()) return (Double) null;*/
-        return (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+        //System.out.println((x2+1 - x1));
+		if((x2-x1!=0.0) && ((y2-y1!=0.0)))
+		return (y2 - y1) / (x2 - x1);
+		
+		else
+		return (y2+0.1 - y1) / (x2+0.1 - x1);
+
     }
 
   
 		
-	public static Point2D[] topoints(Point2D A, Point2D B){
+	public static double[] topoints(double x1, double y1, double x2, double y2){
 			
 		double a;
 		double b;
-		a=getPerpendiculaire(A, B)[0];
-		b=getPerpendiculaire(A, B)[1];
+		a=getPerpendiculaire(x1, y1, x2, y2)[0];
+		b=getPerpendiculaire(x1, y1, x2, y2)[1];
 		
-		double x=(B.getX()+5);
-		double x2=(B.getX()-5);
+		double x=(x2+5);
+		double xx=(x2-5);
 		
 		double y= a*x+b;
-		double y2= a*x2+b;
+		double yy= a*xx+b;
 
-		return new Point2D[]{new Point((int) x,(int) y),new Point((int) x2,(int) y2)};
+		return new double[]{x,y,xx,yy};
 			
 	}
 	
