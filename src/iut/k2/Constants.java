6b35e0d3f98a9ec41bdc96bcf7f33fcc89fd740e
Noxilex <dhoop59@hotmail.fr>
2015-10-13 15:10:28 +0200

    Working on LevelTest

--- src/iut/k2/Constants.java
+++ src/iut/k2/Constants.java
@@ -4,6 +4,6 @@ package iut.k2;
  * Created by Nicolas Beaussart on 13/10/15 for angryBrids.
  */
 public class Constants {
-    static final int SIZE_HEIGHT = 400;
-    static final int SIZE_WIDE = 600;
+    public static final int SIZE_HEIGHT = 400;
+    public static final int SIZE_WIDE = 600;
 }
--- src/iut/k2/data/LevelTest.java
+++ src/iut/k2/data/LevelTest.java
@@ -1,7 +1,11 @@
 package iut.k2.data;
 
+import iut.k2.Constants;
 import iut.k2.physics.Coordinate2D;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 public class LevelTest extends Level{
@@ -12,14 +16,39 @@ public class LevelTest extends Level{
 	public void init() {
 		// TODO Auto-generated method stub
 		Random r = new Random();
-		getLsEntitys().add(new Pecker(new Coordinate2D(0,0)));
 		int nbObstacles = r.nextInt(6)+5;
 		
+		//Instancie les listes
+		setLsEntitys(new ArrayList<Entity>());
+		setLsObjects(new HashMap<Integer, List<AbstractGameObject>>());
+		
+		//Création de l'oiseau
+		Pecker p = new Pecker(new Coordinate2D(0,0));
+		
+			//Ajout dans la liste entity
+			getLsEntitys().add(p);
+			//Ajout dans la map AbstractGameObject
+			List<AbstractGameObject> listePecker = new ArrayList<AbstractGameObject>();
+			listePecker.add(p);
+			getLsObjects().put(1, listePecker);
+		
 		for(int i = 0; i < nbObstacles; i++){
-			int x = r.nextInt(Constants.SIZE_WIDE/2)+largEcran/2;
-			int y = r.nextInt(hautEcran);
-			getLsEntitys().add(new Obstacle(new Coordinate2D(x, y)));
+			//Coordonnées de l'obstacle
+			int x = r.nextInt(Constants.SIZE_WIDE/2)+Constants.SIZE_WIDE/2;
+			int y = r.nextInt(Constants.SIZE_HEIGHT);
+			
+			//Création d'un obstacle
+			Obstacle o = new Obstacle(new Coordinate2D(x, y));
+			
+			//Ajout de l'obstacle dans la liste entity
+			getLsEntitys().add(o);
+			
+			//Ajout de l'obstace dans la map AbstractGameObject
+			
+			
 		}
+		
+		
 		
 	}
 
