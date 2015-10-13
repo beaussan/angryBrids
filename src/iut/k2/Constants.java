bfc683a58c0b7bb43fe6b50ca87a4b0e1ae45d9c
Nicolas Beaussart <nic.beaussart@gmail.com>
2015-10-13 14:54:32 +0200

    ADding constants

---
+++ src/iut/k2/Constants.java
@@ -0,0 +1,9 @@
+package iut.k2;
+
+/**
+ * Created by Nicolas Beaussart on 13/10/15 for angryBrids.
+ */
+public class Constants {
+    static final int SIZE_HEIGHT = 400;
+    static final int SIZE_WIDE = 600;
+}
