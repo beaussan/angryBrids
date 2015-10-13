f71ac785e145145553fe14b5172258bad7fd9cb2
Nicolas Beaussart <nic.beaussart@gmail.com>
2015-10-13 10:03:26 +0200

    FIxing bec thing !

--- src/iut/k2/gui/renderfunc/DrawBird.java
+++ src/iut/k2/gui/renderfunc/DrawBird.java
@@ -10,20 +10,24 @@ import java.awt.geom.Point2D;
 public class DrawBird {
 
     public static final int SIZE_BIRD = 20;
+    public static final int SIEE_ARROW = 30;
+    public static final int SIZE_SIDE = 10;
+    public static final Color COLOR_ARROW = Color.ORANGE;
+    public static final Color COLOR_BODY = Color.RED;
+    public static boolean IS_SKELETON = false;
 
     public static void drawBird(Graphics g, double x, double y, double xNext, double yNext) {
         Color c = g.getColor();
-        g.setColor(Color.RED);
+        g.setColor(COLOR_BODY);
 
 
-        g.drawOval((int) x - (SIZE_BIRD / 2), (int) y - (SIZE_BIRD / 2), SIZE_BIRD, SIZE_BIRD);
         double angle;
         angle = getAngle(x, y, xNext, yNext);
 
         //System.out.println(angle);
 
-        int xTo = (int) x + (int) (40 * Math.cos(angle));
-        int yTo = (int) y + (int) (40 * Math.sin(angle));
+        int xTo = (int) x + (int) (SIEE_ARROW * Math.cos(angle));
+        int yTo = (int) y + (int) (SIEE_ARROW * Math.sin(angle));
 
   /*      if (xTo < x) {
             xTo = x + (int) (40 * Math.cos(angle + Math.PI));
@@ -32,28 +36,50 @@ public class DrawBird {
 
         int lineX = (int) x;
         int lineY = (int) y;
-        g.drawLine(lineX, lineY, xTo, yTo);
         
         /*int[] x2={(int)x,(int)x,(int)x+10};
 		int[] y2={(int)(y-20),(int)y,(int)(y-10)};
 		g.drawPolygon(x2,y2,x2.length);
 		g.fillPolygon(x2,y2,x2.length);*/
-    	
-        
-        
-        
+
+
         Point2D h = new Point(xTo, yTo);
 		Point2D i = new Point(lineX, lineY);
 				
 		Point2D e = topoints(h,i)[0];
 		Point2D f = topoints(h,i)[1];
+
+        double ang = getAngle(x, y, e.getX(), e.getY());
+
+        int xBot = (int) x + (int) (SIZE_SIDE * Math.cos(ang));
+        int yBot = (int) y + (int) (SIZE_SIDE * Math.sin(ang));
+
+        double ang2 = getAngle(x, y, f.getX(), f.getY());
+
+        int xTop = (int) x + (int) (SIZE_SIDE * Math.cos(ang2));
+        int yTop = (int) y + (int) (SIZE_SIDE * Math.sin(ang2));
+
+        //int xTo = (int) x + (int) (40 * Math.cos(angle));
+        //int yTo = (int) y + (int) (40 * Math.sin(angle));
+        g.setColor(COLOR_ARROW);
+        if (IS_SKELETON) {
+            g.drawLine(lineX, lineY, xTo, yTo);
+            g.drawLine(xBot, yBot, xTop, yTop);
+            g.drawPolygon(new int[]{xTo, xTop, xBot}, new int[]{yTo, yTop, yBot}, 3);
+        } else {
+            g.fillPolygon(new int[]{xTo, xTop, xBot}, new int[]{yTo, yTop, yBot}, 3);
+        }
+
+       /* g.drawLine((int) x - (SIZE_BIRD / 2), (int) y - (SIZE_BIRD / 2), (int)x, (int)y);*/
+
+
    		/*double a = (i.getY()-h.getY()) / (i.getX()-h.getX());
-    	double coef = -1 / a;
+        double coef = -1 / a;
     	double pos  = i.getY() - coef * i.getX();
-    	
+
     	double y3= coef*(i.getX()+10)+pos;
 		double y4= coef*(i.getX()-10)+pos;
-		
+
         g.drawLine((int)(i.getX()+10), (int)y3, (int)(i.getX()-10), (int)y4);*/
 
         /*double angle2 = getAngle((int)e.getX(), (int)e.getY(), (int)f.getX(), (int)f.getY());
@@ -63,10 +89,12 @@ public class DrawBird {
 		int[] y2={(int)h.getY(),(int)e.getY(),(int)f.getY()};*/
 		/*g.drawPolygon(x2,y2,x2.length);
 		g.fillPolygon(x2,y2,x2.length);*/
-        
-        g.drawLine((int)e.getX(), (int)e.getY(), (int)f.getX(), (int)f.getY());
-       /* g.drawLine((int) x - (SIZE_BIRD / 2), (int) y - (SIZE_BIRD / 2), (int)x, (int)y);*/
-
+        g.setColor(COLOR_BODY);
+        if (IS_SKELETON) {
+            g.drawOval((int) x - (SIZE_BIRD / 2), (int) y - (SIZE_BIRD / 2), SIZE_BIRD, SIZE_BIRD);
+        } else {
+            g.fillOval((int) x - (SIZE_BIRD / 2), (int) y - (SIZE_BIRD / 2), SIZE_BIRD, SIZE_BIRD);
+        }
 
         g.setColor(c);
     }
@@ -92,9 +120,8 @@ public class DrawBird {
 	
 	public static double lineEquation(Point2D p1, Point2D p2) {
 		/*if (p1.getX() == p2.getX()) return (Double) null;*/
-		double a = (p2.getY()-p1.getY()) / (p2.getX()-p1.getX());
-		return a;
-	}
+        return (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+    }
 
   
 		
