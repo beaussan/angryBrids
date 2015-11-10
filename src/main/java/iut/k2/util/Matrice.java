ae7e5657d178f60c87b850bc6d49bb57e9d6ad0e
Benjamin D'HOOP <dhoop59@hotmail.fr>
2015-11-10 13:27:32 +0100

    Problem in my directory due to removal of files and import of com.google

---
+++ src/main/java/iut/k2/util/Matrice.java
@@ -0,0 +1,45 @@
+package main;
+
+public class Matrice {
+	
+	double tab[][];
+	
+	public Matrice(int largeur, int hauteur){
+		tab = new double[hauteur][largeur];
+	}
+	
+	public Matrice() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void insert(double valeur, int x, int y){
+		tab[y][x] = valeur;
+	}
+	
+	public double getValue(int x, int y){
+		return tab[y][x];
+	}
+	
+	public double[][] getAllValues(){
+		return tab;
+	}
+	
+	public int getHauteur(){
+		return tab.length;
+	}
+	
+	public int getLargeur(){
+		return tab[0].length;
+	}
+	
+	public String toString(){
+		String res = "";
+		for(int hauteur = 0; hauteur < tab.length; hauteur++){
+			for(int largeur = 0; largeur < tab[0].length; largeur++){
+				res+= tab[hauteur][largeur] + " ";
+			}
+			res+="\n";
+		}
+		return res;
+	}
+}
